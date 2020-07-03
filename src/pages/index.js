b861ee1d81b2abf5fb0ed10a0217c44e6e7020a3
Brian Lee <engbrianlee@gmail.com>
2020-07-03 19:40:57 -0400

    Add animation to header

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import SEO from "../components/SEO";
 import Layout from "../components/layout/Layout";
 import Hero from "../components/pages/index/Hero";
-import Resume from "../components/pages/index/Resume";
-import Projects from "../components/pages/index/Projects";
+import Resume from "../components/pages/index/resume/Resume";
+import Projects from "../components/pages/index/projects/Projects";
 
 const HomePage = () => {
   return (
